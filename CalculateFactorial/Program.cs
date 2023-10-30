bffdf4a60150041f5f31e35cd2163a2aa454c4be
ibrahimacar220 <iacar8800@gmail.com>
2023-10-30 14:12:22 +0300

    Add project files.

---
+++ CalculateFactorial/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number = Convert.ToInt32( Console.ReadLine());
+
+ int fnc(int x)
+{
+
+    for (int i = 0; i < x; i++)
+    {
+        if (x == 1)
+        {
+            return 1;
+
+        }
+        else
+        {
+            return x * fnc(x - 1);
+        }
+    }
+    return 0;
+
+}
+
+Console.WriteLine(fnc(number));
+Console.ReadLine();
